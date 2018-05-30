5839d67949d55b528c20c3242f2fb8e600e02f15
GARfIlDCat <v.a.bogush@gmail.com>
2018-05-30 16:58:22 +0300

    Начало работы с FileBrowser

--- app/src/main/java/by/bos_soft/tablereader/FileBrowser.java
+++ app/src/main/java/by/bos_soft/tablereader/FileBrowser.java
@@ -2,12 +2,62 @@ package by.bos_soft.tablereader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileBrowser extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    // имена атрибутов для Map
+    final String ATTRIBUTE_NAME_TEXT = "text";
+    final String ATTRIBUTE_NAME_CHECKED = "checked";
+    final String ATTRIBUTE_NAME_IMAGE = "image";
+
+    ListView lvSimple;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_browser);
+
+        // массив имен атрибутов, из которых будут читаться данные
+        String[] from = { ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_CHECKED,
+                ATTRIBUTE_NAME_IMAGE };
+        // массив ID View-компонентов, в которые будут вставлять данные
+        int[] to = { R.id.tvText, R.id.cbChecked, R.id.ivImg };
+
+        ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+        data = FillData();
+
+        // создаем адаптер
+        SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.item_file_browser,
+                from, to);
+
+        // определяем список и присваиваем ему адаптер
+        lvSimple = (ListView) findViewById(R.id.lvFiles);
+        lvSimple.setAdapter(sAdapter);
     }
+
+    public ArrayList<Map<String, Object>> FillData() {
+// массивы данных
+        String[] texts = { "sometext 1", "sometext 2", "sometext 3",
+                "sometext 4", "sometext 5", "some 6" };
+        boolean[] checked = { true, false, false, true, false, true };
+        int img = R.mipmap.ic_launcher;
+
+        // упаковываем данные в понятную для адаптера структуру
+        ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(texts.length);
+        Map<String, Object> m;
+        for (int i = 0; i < texts.length; i++) {
+            m = new HashMap<String, Object>();
+            m.put(ATTRIBUTE_NAME_TEXT, texts[i]);
+            m.put(ATTRIBUTE_NAME_CHECKED, checked[i]);
+            m.put(ATTRIBUTE_NAME_IMAGE, img);
+            data.add(m);
+        }
+        return data;
+    }
+
 }
