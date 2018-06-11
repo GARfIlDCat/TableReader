1d9af90a9ca4074c1d0a4387602d235c6c35b856
GARfIlDCat <v.a.bogush@gmail.com>
2018-06-11 23:23:38 +0300

    Переделал на фрагменты

--- app/src/main/java/by/bos_soft/tablereader/MainActivity.java
+++ app/src/main/java/by/bos_soft/tablereader/MainActivity.java
@@ -1,15 +1,17 @@
 package by.bos_soft.tablereader;
 
-import android.content.Intent;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity implements FileBrowserFragment.OnFragmentInteractionListener {
 
     Button btnFileBrowser;
+    FileBrowserFragment frFileBrowser;
+    FragmentTransaction fTrans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +19,21 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         setContentView(R.layout.activity_main);
 
         btnFileBrowser = (Button) findViewById(R.id.btnFileBrowser);
-        btnFileBrowser.callOnClick();
+        frFileBrowser = new FileBrowserFragment();
+    }
+
+    public void onClick(View v) {
+        fTrans = getFragmentManager().beginTransaction();
+        switch (v.getId()) {
+            case R.id.btnFileBrowser:
+                fTrans.add(R.id.frCont, frFileBrowser);
+                break;
+        }
+        fTrans.commit();
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnFileBrowser:
-                startActivity(new Intent(this, FileBrowser.class));
-                break;
-            default:
-                break;
-        }
-    }
+    public void onFragmentInteraction(Uri uri) {
 
+    }
 }
